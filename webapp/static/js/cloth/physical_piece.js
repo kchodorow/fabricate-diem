0145bb9d75cc55e1b23a1a09e61a6716ecbc9dad
Kristina Chodorow <k.chodorow@gmail.com>
2016-12-20 07:09:02 -0500

    Using a "normal" geometry works
    Unless the shape changes.
    
    #41

--- webapp/static/js/cloth/physical_piece.js
+++ webapp/static/js/cloth/physical_piece.js
@@ -25,7 +25,17 @@ diem.cloth.PhysicalPiece = function(piece, clothWidth, clothHeight) {
   this.handle_ = 0;
   this.mouse_ = null;
 
-  this.mesh_ = this.createMesh_(piece);
+  var geometry = this.createGeometry_(piece.geometry);
+  var clothPos = new THREE.Vector3().copy(piece.position);
+  geometry.translate(clothPos.x, clothPos.y, 0);
+  var clothMaterial = new THREE.MeshLambertMaterial({
+    color: piece.material.color,
+    side: THREE.DoubleSide
+  });
+  this.mesh_ = new THREE.Mesh(geometry, clothMaterial);
+  this.mesh_.castShadow = true;
+  this.mesh_.receiveShadow = true;
+
   var softBody = this.createSoftBody_(this.mesh_.geometry);
   this.geometryMapper_ = new diem.cloth.GeometryMapper(softBody);
   diem.Physics.get().getWorld().addSoftBody(softBody);
@@ -37,46 +47,24 @@ goog.inherits(diem.cloth.PhysicalPiece, diem.MeshWrapper);
 diem.cloth.PhysicalPiece.pieces_ = [];
 
 /**
- * @param {THREE.Mesh} mesh
- * @returns {THREE.Mesh}
- * @private
- */
-diem.cloth.PhysicalPiece.prototype.createMesh_ = function(mesh) {
-  var clothGeometry = this.createIndexedBufferGeometry_(mesh.geometry);
-  var clothPos = new THREE.Vector3().copy(mesh.position);
-  clothGeometry.translate(clothPos.x, clothPos.y, 0);
-  var clothMaterial = new THREE.MeshBasicMaterial({
-    color: mesh.material.color,
-    side: THREE.DoubleSide,
-    wireframe: true
-  });
-  var newMesh = new THREE.Mesh(clothGeometry, clothMaterial);
-  newMesh.castShadow = true;
-  newMesh.receiveShadow = true;
-  return newMesh;
-};
-
-/**
  * @param {THREE.Mesh} newMesh
  * @private
  */
 diem.cloth.PhysicalPiece.prototype.updateGeometry = function(newMesh) {
+  var geometry = this.createGeometry_(newMesh.geometry);
+  this.mesh_.geometry = geometry;
+
   var oldSoftBody = this.mesh_.userData.physicsBody;
   diem.Physics.get().getWorld().removeSoftBody(oldSoftBody);
 
-  var buffMesh = this.createMesh_(newMesh);
-  var newSoftBody = this.createSoftBody_(buffMesh.geometry);
+  var newSoftBody = this.createSoftBody_();
   this.geometryMapper_.flip(newSoftBody);
   diem.Physics.get().getWorld().addSoftBody(newSoftBody);
 
-  var parent = this.mesh_.parent;
-  parent.remove(this.mesh_);
-  this.mesh_ = buffMesh;
-  this.mesh_.userData.physicsBody = newSoftBody;
-  parent.add(this.mesh_);
-
   for (var i = 0; i < this.pinned_.length; ++i) {
     var pin = this.pinned_[i];
+    // TODO: actually put in the right index, don't just match it with its
+    // old index.
     newSoftBody.appendAnchor(pin.index(), pin.rigidBody(), false, 1);
   }
 };
@@ -86,16 +74,14 @@ diem.cloth.PhysicalPiece.prototype.updateGeometry = function(newMesh) {
  * @returns {Ammo.btSoftBody}
  * @private
  */
-diem.cloth.PhysicalPiece.prototype.createSoftBody_ = function(geometry) {
+diem.cloth.PhysicalPiece.prototype.createSoftBody_ = function() {
   var helper = new Ammo.btSoftBodyHelpers();
-  // The btSoftBody is centered at (0,0), so its corners should be offset
-  // by the position of the cloth.
-  // Weirdness: why is 1,0 the llc?
+  this.createAmmoArrays_();
   var softBody = helper.CreateFromTriMesh(
     diem.Physics.get().getWorld().getWorldInfo(),
-    geometry.attributes.position.array,
-    geometry.index.array,
-    geometry.index.array.length / 3,
+    this.mesh_.userData.vertices,
+    this.mesh_.userData.indices,
+    this.mesh_.userData.indices.length / 3,
     true);
   softBody.setTotalMass(0.9, false);
   // Disable deactivation
@@ -116,49 +102,44 @@ diem.cloth.PhysicalPiece.prototype.createSoftBody_ = function(geometry) {
  * @returns {THREE.BufferGeometry}
  * @private
  */
-diem.cloth.PhysicalPiece.prototype.createIndexedBufferGeometry_ = function(geometry) {
+diem.cloth.PhysicalPiece.prototype.createGeometry_ = function(geometry) {
   geometry = geometry.clone();
   var subdivider = new THREE.SubdivisionModifier(3);
   subdivider.modify(geometry);
   geometry.subdivided = true;
   goog.asserts.assert(geometry.vertices.length < 100000);
+  return geometry;
+};
 
-  var numVertices = geometry.vertices.length;
-  var numFaces = geometry.faces.length;
+diem.cloth.PhysicalPiece.prototype.createAmmoArrays_ = function() {
+  var numVertices = this.mesh_.geometry.vertices.length;
+  var numFaces = this.mesh_.geometry.faces.length;
 
-  var bufferGeom = new THREE.BufferGeometry();
-  var vertices = new Float32Array(numVertices * 3);
-  var normals = new Float32Array(numVertices * 3);
-  var indices = new Uint16Array(numFaces * 3);
+  var ammoVertices = new Float32Array(numVertices * 3);
+  var ammoIndices = new Uint16Array(numFaces * 3);
 
   var jitter = .01;
   for (var i = 0; i < numVertices; i++) {
-    var p = geometry.vertices[ i ];
+    var p = this.mesh_.geometry.vertices[i];
     var i3 = i * 3;
 
-    vertices[i3] = p.x;
-    vertices[i3 + 1] = p.y;
+    ammoVertices[i3] = p.x;
+    ammoVertices[i3 + 1] = p.y;
     // Offset z slightly, so the cloth isn't stuck on the plane.
-    vertices[i3 + 2] = jitter;
+    ammoVertices[i3 + 2] = jitter;
     jitter *= -1;
-    normals[i3] = 0;
-    normals[i3 + 1] = 0;
-    normals[i3 + 2] = 1;
   }
 
-  for (i = 0; i < numFaces; i++) {
-    var f = geometry.faces[ i ];
+  for (i = 0; i < numFaces; ++i) {
+    var f = this.mesh_.geometry.faces[i];
     i3 = i * 3;
-
-    indices[ i3 ] = f.a;
-    indices[ i3 + 1 ] = f.b;
-    indices[ i3 + 2 ] = f.c;
+    ammoIndices[i3] = f.a;
+    ammoIndices[i3 + 1] = f.b;
+    ammoIndices[i3 + 2] = f.c;
   }
 
-  bufferGeom.setIndex(new THREE.BufferAttribute(indices, 3));
-  bufferGeom.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
-  bufferGeom.addAttribute('normal', new THREE.BufferAttribute(normals, 3));
-  return bufferGeom;
+  this.mesh_.userData.vertices = ammoVertices;
+  this.mesh_.userData.indices = ammoIndices;
 };
 
 /**
@@ -188,36 +169,38 @@ diem.cloth.PhysicalPiece.prototype.getIntersectables = function() {
 diem.cloth.PhysicalPiece.prototype.simulate = function() {
   var indexFloat = 0;
   var geometry = this.mesh_.geometry;
-  var volumePositions = geometry.attributes.position.array;
-  var volumeNormals = geometry.attributes.normal.array;
+  var positions = geometry.vertices;
 
   var softBody = this.mesh_.userData.physicsBody;
   var nodes = softBody.get_m_nodes();
   for (var j = 0; j < nodes.size(); ++j) {
+    var pos = positions[j];
     var node = nodes.at(j);
     var nodePos = node.get_m_x();
-    var x = nodePos.x();
-    var y = nodePos.y();
-    var z = nodePos.z();
-    var nodeNormal = node.get_m_n();
-    var nx = nodeNormal.x();
-    var ny = nodeNormal.y();
-    var nz = nodeNormal.z();
-
-    var indexVertex = j * 3;
-    volumePositions[indexVertex] = x;
-    volumeNormals[indexVertex] = nx;
-    indexVertex++;
-    volumePositions[indexVertex] = y;
-    volumeNormals[indexVertex] = ny;
-    indexVertex++;
-    volumePositions[indexVertex] = z;
-    volumeNormals[indexVertex] = nz;
+    pos.x = nodePos.x();
+    pos.y = nodePos.y();
+    pos.z = nodePos.z();
   }
 
-  geometry.attributes.position.needsUpdate = true;
-  geometry.attributes.normal.needsUpdate = true;
+  // TODO: this is super inefficient, we could have each face normal be a
+  // reference, not a copy.
+  for (var i = 0; i < geometry.faces.length; ++i) {
+    var face = geometry.faces[i];
+    var normals = face.vertexNormals;
+    diem.cloth.PhysicalPiece.toVector3(nodes.at(face.a).get_m_n(), normals[0]);
+    diem.cloth.PhysicalPiece.toVector3(nodes.at(face.b).get_m_n(), normals[1]);
+    diem.cloth.PhysicalPiece.toVector3(nodes.at(face.c).get_m_n(), normals[2]);
+  }
+
+  geometry.verticesNeedUpdate = true;
+  geometry.normalsNeedUpdate = true;
   this.mesh_.geometry.boundingSphere = null;
+};
+
+diem.cloth.PhysicalPiece.toVector3 = function(btVec3, vec3) {
+  vec3.x = btVec3.x();
+  vec3.y = btVec3.y();
+  vec3.z = btVec3.z();
 };
 
 /**
