cf54d0b4eb8bcf474e88fd1f769650dc6fab0d95
Kristina Chodorow <k.chodorow@gmail.com>
2018-04-12 13:23:37 -0400

    Remove unnecessary import

--- src/app/user.service.spec.ts
+++ src/app/user.service.spec.ts
@@ -1,4 +1,4 @@
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed, inject } from '@angular/core/testing';
 
 import { UserService } from './user.service';
